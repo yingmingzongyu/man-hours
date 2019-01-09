1242e5c8b628798fc7e21603acde3466e6a609fa
Zero <a958787c@qq.com>
2019-01-09 17:04:55 +0800

    first init

---
+++ src/view/single-page/home/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Hello!</p>
+  </div>
+</template>
+
+<script>
+import InforCard from '_c/info-card'
+import CountTo from '_c/count-to'
+import { ChartPie, ChartBar } from '_c/charts'
+import Example from './example.vue'
+export default {
+  name: 'home',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    //
+  }
+}
+</script>
+
+<style lang="less">
+.count-style{
+  font-size: 50px;
+}
+</style>
