887d6ebe0b3626b3e99335f9eb057d91876c6332
zj9495 <zj9495@gmail.com>
2019-01-10 18:02:36 +0800

    add标签管理菜单与路由

---
+++ src/view/project/software-development/manage-content/index.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Author: yincheng
+ * @Date: 2019-01-10 17:58:57
+ * @LastEditors: yincheng
+ * @LastEditTime: 2019-01-10 18:01:46
+ -->
+<template>
+
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
