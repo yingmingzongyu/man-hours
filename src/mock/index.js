a4ea6665780045eb6cb876a37c8a96657dc762b3
zj9495 <zj9495@gmail.com>
2019-01-10 17:36:56 +0800

    add Mock数据

--- src/mock/index.js
+++ src/mock/index.js
@@ -3,6 +3,8 @@ import { login, logout, getUserInfo } from './login'
 import { getTableData, getDragList, uploadImage, getOrgData } from './data'
 import { getMessageInit, getContentByMsgId, hasRead, removeReaded, restoreTrash, messageCount } from './user'
 
+import { addProject, updateProject, projectDetails, queryDetails, addLabel, delLabel, queryLabel } from './project'
+
 // 配置Ajax请求延时，可用来测试网络延迟大时项目中一些效果
 Mock.setup({
   timeout: 1000
@@ -23,5 +25,14 @@ Mock.mock(/\/message\/remove_readed/, removeReaded)
 Mock.mock(/\/message\/restore/, restoreTrash)
 Mock.mock(/\/message\/count/, messageCount)
 Mock.mock(/\/get_org_data/, getOrgData)
+
+
+Mock.mock(/\/project\/management\/add/, addProject)
+Mock.mock(/\/project\/management\/update/, updateProject)
+Mock.mock(/\/project\/management\/details/, projectDetails)
+Mock.mock(/\/project\/management\/query/, queryDetails)
+Mock.mock(/\/project\/label\/add/, addLabel)
+Mock.mock(/\/project\/label\/delete/, delLabel)
+Mock.mock(/\/project\/label\/query/, queryLabel)
 
 export default Mock
---
+++ src/mock/project.js
@@ -0,0 +1,147 @@
+import Mock from 'mockjs'
+import { doCustomTimes } from '@/libs/util'
+const Random = Mock.Random
+
+export const addProject = () => {
+  return {
+    status: 200,
+    message: '新增成功'
+  }
+}
+
+export const updateProject = () => {
+  return {
+    status: 200,
+    message: '修改成功'
+  }
+}
+
+export const projectDetails = () => {
+  const data = Mock.mock({
+    "projectNumber|1-300000": 3,
+    "projectAttribution|1": ["ZHX", "GX", "PT"],
+    "projectType|1-3": 3,
+    "businessType|1-8": 8,
+    "projectName": "@cword(3, 10)",
+    "timeEvaluation|1-100": 100,
+    "labelId|1": ["1", "2", "5", "1,3,5", "1,2,4"],
+    "summarize": "@cword(4, 20)",
+    "startTime": "@datetime",
+    "endTime": "@datetime",
+    "participants": "张三、李四"
+  })
+  return {
+    status: 200,
+    data
+  }
+}
+export const queryDetails = req => {
+  let list = []
+  req = JSON.parse(req.body)
+  doCustomTimes(10, () => {
+    list.push(Mock.mock({
+      "projectNumber|1-300000": 3,
+      "projectName": "@cword(3, 10)",
+      "projectAttribution|1": ["ZHX", "GX", "PT"],
+      "labelId|1": ["1", "2", "5", "1,3,5", "1,2,4"],
+      "labelName|1": ["test1", "test2", "test5", "test1,test3,test5", "test1,test2,test4"],
+      "summarize": "@cword(4, 20)",
+      "createTime": "@datetime",
+    }))
+  })
+  return {
+    status: 200,
+    data: {
+      list,
+      total: 999,
+      pageSize: 10,
+      pageNum: req.pageNum,
+      pages: 10,
+    }
+  }
+}
+export const addLabel = () => {
+  return {
+    "status": 200,
+    "message": "标签绑定成功"
+  }
+}
+export const delLabel = () => {
+  return {
+    "status": 200,
+    "message": "标签删除成功"
+  }
+}
+export const queryLabel = req => {
+  let list = []
+  req = JSON.parse(req.body)
+  doCustomTimes(10, () => {
+    list.push(Mock.mock({
+      "relatedProjectsCount|1-30": 30,
+      "id|1-30": 30,
+      "labelName|1": "@cword(1, 8)",
+    }))
+  })
+  return {
+    status: 200,
+    data: {
+      list,
+      total: 23,
+      pageSize: 10,
+      pageNum: req.pageNum,
+      pages: 2,
+    }
+  }
+
+}
+// export const getMessageInit = () => {
+//   let unreadList = []
+//   doCustomTimes(3, () => {
+//     unreadList.push(Mock.mock({
+//       title: Random.cword(10, 15),
+//       create_time: '@date',
+//       msg_id: Random.increment(100)
+//     }))
+//   })
+//   let readedList = []
+//   doCustomTimes(4, () => {
+//     readedList.push(Mock.mock({
+//       title: Random.cword(10, 15),
+//       create_time: '@date',
+//       msg_id: Random.increment(100)
+//     }))
+//   })
+//   let trashList = []
+//   doCustomTimes(2, () => {
+//     trashList.push(Mock.mock({
+//       title: Random.cword(10, 15),
+//       create_time: '@date',
+//       msg_id: Random.increment(100)
+//     }))
+//   })
+//   return {
+//     unread: unreadList,
+//     readed: readedList,
+//     trash: trashList
+//   }
+// }
+
+// export const getContentByMsgId = () => {
+//   return `<divcourier new',="" monospace;font-weight:="" normal;font-size:="" 12px;line-height:="" 18px;white-space:="" pre;"=""><div>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: medium;">这是消息内容，这个内容是使用<span style="color: rgb(255, 255, 255); background-color: rgb(28, 72, 127);">富文本编辑器</span>编辑的，所以你可以看到一些<span style="text-decoration-line: underline; font-style: italic; color: rgb(194, 79, 74);">格式</span></span></div><ol><li>你可以查看Mock返回的数据格式，和api请求的接口，来确定你的后端接口的开发</li><li>使用你的真实接口后，前端页面基本不需要修改即可满足基本需求</li><li>快来试试吧</li></ol><p>${Random.csentence(100, 200)}</p></divcourier>`
+// }
+
+// export const hasRead = () => {
+//   return true
+// }
+
+// export const removeReaded = () => {
+//   return true
+// }
+
+// export const restoreTrash = () => {
+//   return true
+// }
+
+// export const messageCount = () => {
+//   return 3
+// }
